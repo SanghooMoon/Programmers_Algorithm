2b00005e3249f89f20772d67589a646737e2d783
SanghooMoon <sanghoo.moon@gmail.com>
2020-09-04 22:59:50 +0900

    2020-09-04

---
+++ src/programmers/practiceQuiz/제일작은수제거하기.java
@@ -0,0 +1,45 @@
+package programmers.practiceQuiz;
+
+import java.util.Arrays;
+
+/**
+ * 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 
+ * 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 
+ * 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+ */
+public class 제일작은수제거하기 {
+	
+	public static int[] solution(int[] arr) {
+        int[] answer = new int[arr.length-1];
+        int minIndex = 0;
+        int min = arr[0];
+        
+        if(arr.length < 2) 
+        	return new int[] {-1};
+        
+        for(int i=0; i<arr.length; i++) {
+        	if(min > arr[i]) {
+        		minIndex = i;
+        		min = arr[i];
+        	}
+        }
+        
+        int index = 0;
+        for(int i=0; i<arr.length; i++) {
+        	if(i==minIndex)
+        		continue;
+        	answer[index++] = arr[i];
+        }
+        
+        System.out.println(minIndex);
+        System.out.println(min);
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		int[] arr = {4,3,2,1,1};
+		solution(arr);
+	}
+
+}
