b4fd7723db7c174aaec05b8482e931b0c10f03c5
SanghooMoon <sanghoo.moon@gmail.com>
2020-12-01 19:29:24 +0900

    백준 - 3단계[for문]

---
+++ src/baekjoon/for문/A더하기B빼기8.java
@@ -0,0 +1,31 @@
+package baekjoon.for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+public class A더하기B빼기8 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int input = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=input; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			bw.append("Case #"+i+": ");
+			bw.append(a + " + " + b + " = " + (a+b) + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
---
+++ src/baekjoon/for문/N찍기.java
@@ -0,0 +1,23 @@
+package baekjoon.for문;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+public class N찍기 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try {
+			int input = Integer.parseInt(br.readLine());
+			for(int i=1; i<=input; i++)
+				bw.write(i+"\n");
+			bw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+}
