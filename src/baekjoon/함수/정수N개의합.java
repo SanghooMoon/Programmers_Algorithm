16d3059f9806c7287fe91976a2395a33b2d3a4f8
SanghooMoon <sanghoo.moon@gmail.com>
2020-12-05 22:08:05 +0900

    백준 - 6단계[함수]

---
+++ src/baekjoon/함수/정수N개의합.java
@@ -0,0 +1,19 @@
+package baekjoon.함수;
+
+// 정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
+public class 정수N개의합 {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4};
+		sum(arr);
+	}
+	
+	public static long sum(int[] a) {
+		long sum = 0;
+		for(int i : a) {
+			sum += i;
+		}
+		return sum;
+	}
+
+}
